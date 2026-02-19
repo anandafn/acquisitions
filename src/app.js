38bf7c2991d15160fc69efa40680d111c5dccd7e
anandafn <anandanamira03@gmail.com>
2026-02-19 17:13:40 +0700

    update

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 import express from 'express';
+import logger from '#config/logger.js';
+import helmet from 'helmet';
+import morgan from 'morgan';
 
 const app = express();
 
+app.use(helmet());
+
 app.get('/', (req, res) => {
-  res.status(200).send('Hello, World!');
+  logger.info('Hello, from acquisitions');
+  res.status(200).send('Hello, from acquisitions');
 });
 
 export default app;
