29f0af47860f128a11ba9b5526863a4ec514a236
anandafn <anandanamira03@gmail.com>
2026-02-20 12:01:19 +0700

    add arcjet

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 import 'dotenv/config';
 
-import { neon, neonConfig } from '@neondatabase/serverless';
+import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 
-export const sql = neon(process.env.DATABASE_URL);
+const sql = neon(process.env.DATABASE_URL);
 
 const db = drizzle(sql);
 
